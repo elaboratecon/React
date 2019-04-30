ea069208fe4218c233cc5033cb2ec176351b2d31
Jason Conover <JasonConover@outlook.com>
2019-04-29 17:47:41 -0700

    adding changes

--- app/components/MyWork.js
+++ app/components/MyWork.js
@@ -1,7 +1,43 @@
-import React from 'react'
+// Import React components
+import React, { useEffect } from 'react'
 
-export const MyWork = props => (
-  <div className="mywork-container">
-    <h1>My Work</h1>
-  </div>
-)
+// Import custom components
+import { Nasa } from './Nasa'
+import { Adobe } from './Adobe'
+import { Grindr } from './Grindr'
+import { Tmobile } from './TMobile'
+import { ReactJS } from './ReactJS'
+import { AngularJS } from './AngularJS'
+import { WordPress } from './WordPress'
+import { Wyndham } from './Wyndham'
+import { Microsoft } from './Microsoft'
+
+export const MyWork = props => {
+
+  useEffect(
+    () => {
+      const tilesContainer = document.querySelector('.tiles')
+      const tiles = document.querySelectorAll('.tile')
+      tiles.forEach((tile, i, arr) => {
+        tilesContainer.appendChild(tile)
+      })
+    }
+  )
+  
+  return (
+    <div className="mywork-container">
+      <Nasa />
+      <Adobe />
+      <Grindr />
+      <Tmobile />
+      <ReactJS />
+      <AngularJS />
+      <WordPress />
+      <Microsoft />
+      <Wyndham />
+      <nav>
+        <ul className="tiles"></ul>
+      </nav>
+    </div>
+  )
+}
---
+++ app/components/Wyndham.js
@@ -0,0 +1,14 @@
+// Import React components
+import React, { useContext, useEffect } from 'react'
+
+export const Wyndham = () => (
+    <div className="project-wrapper">
+        <li className="tile tile--wyndham">
+            <a href="project/grindr/">Wyndham</a>
+        </li>
+        <div className="project-content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam cursus non ex eget cursus. Ut consequat aliquet magna sit amet suscipit. Sed rhoncus, turpis pretium sodales euismod, arcu arcu sollicitudin augue, vitae vehicula odio orci ac arcu. Mauris a ante vitae neque efficitur vulputate. Suspendisse purus tellus, tempus ac tincidunt eget, tristique a magna. Cras eu tincidunt arcu. Phasellus finibus rhoncus eros. In laoreet sagittis lorem, non iaculis augue ultricies ut. Vestibulum molestie in tortor ut semper. Fusce et mauris eget velit placerat commodo. Sed gravida, massa ultrices venenatis lobortis, augue nibh rhoncus mi, non volutpat turpis dui at nunc. Nam id vestibulum felis, a convallis nisi. Nunc nec malesuada augue, at semper dui. Mauris quam est, efficitur a varius placerat, aliquet et sapien.</p>
+            <p>Duis blandit, leo eget fringilla porttitor, ipsum dui dapibus purus, sit amet viverra diam ante eu sapien. Maecenas in nulla interdum, pellentesque lorem in, aliquet mauris. Sed consectetur odio nec risus condimentum porta. Etiam laoreet odio eget lectus hendrerit ornare. Etiam vitae erat id erat porta tempor. Suspendisse in sodales purus, et fermentum elit. Donec a rhoncus mi. Integer pellentesque erat neque, sed elementum elit sagittis sollicitudin. Aliquam lobortis mi turpis, efficitur rhoncus felis congue sit amet. Pellentesque eu risus efficitur, tincidunt tortor ut, efficitur lorem. Nunc posuere sem rhoncus auctor consectetur. Pellentesque eget sagittis massa, sit amet blandit mi. Nulla sit amet venenatis ex, in laoreet nibh.</p>
+        </div>
+    </div>
+)
